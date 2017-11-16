ec02d362a243b4e06bbafbce84206b4a27e186ec
Sarkar, Arnab(arnasarkar) <arnasarkar@paypal.com>
2017-11-15 21:20:12 -0800

    DFS example solutions

---
+++ src/com/arnab/algos/DFS.java
@@ -0,0 +1,62 @@
+package com.arnab.algos;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class DFS {
+
+	private int v; 
+	private List<Integer> adjacent[];
+
+	DFS(int v) {
+		this.v = v;
+		adjacent = new LinkedList[v];
+		for(int i = 0; i < v; i++) {
+			adjacent[i] = new LinkedList<>();
+		}
+	}
+	
+	public void addEdges(int v, int w) {
+		adjacent[v].add(w);
+	}
+	
+	public void dfsTraverse(int start) {
+		boolean visited[] = new boolean[v];
+		
+		traverse(start, visited);
+	}
+	
+	public void traverse(int start, boolean visited[]) {
+		visited[start] = true;
+		System.out.println(start);
+		Iterator<Integer> iterator = adjacent[start].listIterator();
+		while(iterator.hasNext()) {
+			Integer next = iterator.next();
+			if(!visited[next])
+				traverse(next, visited);
+		}
+	}
+	
+	public static void main(String[] args) {
+		DFS ps = new DFS(13);
+		ps.addEdges(0, 1);
+		ps.addEdges(0, 2);
+		
+		ps.addEdges(1, 3);
+		ps.addEdges(1, 4);
+		ps.addEdges(1, 5);
+		
+		ps.addEdges(2, 6);
+		ps.addEdges(2, 7);
+		ps.addEdges(2, 8);
+		
+		ps.addEdges(6, 9);
+		ps.addEdges(6, 10);
+		ps.addEdges(6, 11);
+		ps.addEdges(6, 12);
+		
+		ps.dfsTraverse(0);
+	}
+
+}
---
+++ src/com/arnab/algos/HappyParent.java
@@ -0,0 +1,93 @@
+package com.arnab.algos;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 
+ * You are given a graph with N vertices and M edges. Master parent is the vertex which has no parent but may have 0 or more children. 
+ * In any connected component of the graph,vertex with the lowest value in that component serves as the master parent. 
+ * A vertex is called happy if it has more children than its parent. Count the number of happy vertices in the given graph.
+ * The graph has no cycles or self loops.
+ *
+ * Input Format:
+ *
+ * First line consists of two space separated integers denoting N and M and the following 
+ * M lines consist of two space separated integers X and Y denoting there is an edge between vertices X and Y.
+ *
+ * Output Format:
+ *
+ * Print the number of happy vertices in the graph.
+ *
+ * @author arnasarkar
+ *
+ */
+class Parent {
+	private int noOfHumans;
+	private List<Integer> humans[];
+	
+	Parent(int noOfHuman) {
+		this.noOfHumans = noOfHuman;
+		humans = new LinkedList[noOfHuman];
+		for(int addHumans = 0; addHumans < noOfHumans; addHumans++) {
+			humans[addHumans] = new LinkedList<Integer>();
+		}
+	}
+	
+	public void addChildren(int parent, int children) {
+		humans[parent].add(children);
+	}
+	
+	public void happyParent(int start) {
+		boolean visited[] = new boolean[noOfHumans];
+		int happyParentList[] = new int[noOfHumans];
+		happyParentList = findHappyParent(visited, start, 0, happyParentList);
+		for(int count = 0; count < noOfHumans;  count++) {
+			if(happyParentList[count] == 1)
+				System.out.println(count);
+		}
+	}
+	
+	public int[] findHappyParent(boolean visited[], int currentHuman, int noOfChildForParent, int happyParentsList[]) {
+		visited[currentHuman] = true;
+		int noOfChildForCurrentParent = humans[currentHuman].size();
+		if(noOfChildForCurrentParent > noOfChildForParent && noOfChildForParent != 0)
+  			happyParentsList[currentHuman] = 1;
+		Iterator<Integer> iterate = humans[currentHuman].listIterator();
+		while(iterate.hasNext()) {
+			Integer child = iterate.next();
+			if(!visited[child])
+				findHappyParent(visited, child, noOfChildForCurrentParent, happyParentsList);
+		}
+		return happyParentsList;
+	}
+	
+}
+
+public class HappyParent {
+
+	public static void main(String[] args) {
+
+		Parent ps = new Parent(13);
+		ps.addChildren(0, 1);
+		ps.addChildren(0, 2);
+		
+		ps.addChildren(1, 3);
+		ps.addChildren(1, 4);
+		ps.addChildren(1, 5);
+		
+		ps.addChildren(2, 6);
+		ps.addChildren(2, 7);
+		ps.addChildren(2, 8);
+		
+		ps.addChildren(6, 9);
+		ps.addChildren(6, 10);
+		ps.addChildren(6, 11);
+		ps.addChildren(6, 12);
+		
+		ps.happyParent(0);
+		
+	}
+
+}
